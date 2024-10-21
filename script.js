daa0365b4af1d85161ccbac655e28ab93072b8f6
bintangmuhamad <mbintangjaya2886@gmail.com>
2024-10-21 16:39:53 +0700

    Attempt 1 - 10.30

--- script.js
+++ script.js
@@ -34,38 +34,5 @@ const generatePassword = (length, options) => {
         const randomIndex = Math.floor(Math.random() * charSet.length);
         password += charSet[randomIndex];
     }
-    return password;
+    return password;
 };
-
-// TODO: Add event listener to the button to call generatePassword and display the output
-document.getElementById('generateBtn').addEventListener('click', () => {
-    const length = parseInt(document.getElementById('length').value, 10);
-    const options = {
-        includeUppercase: document.getElementById('includeUppercase').checked,
-        includeLowercase: document.getElementById('includeLowercase').checked,
-        includeNumbers: document.getElementById('includeNumbers').checked,
-        includeSpecialChars: document.getElementById('includeSpecialChars').checked,
-    };
-
-    const password = generatePassword(length, options);
-    if (password == null) {
-        document.getElementById('passwordOutput').textContent = "Please select at least one criterial!";
-        document.getElementById('passwordOutput').style.color = "red";
-    } else {
-        document.getElementById('passwordOutput').textContent = password;
-        document.getElementById('passwordOutput').style.color = "black";
-    }
-} )
-// BONUS: Implement the copy to clipboard functionality
-document.getElementById('copybtn').addEventListener('clik', () =>{
-    const passwordOutput = document.getElementById('passwordOutput').textContent;
-    if (passwordOutput && passwordOutput !== "Pleasae select at lesat one croteria!") {
-        navigator.clipboard.writeText(passwordOutput).them(() => {
-            alert('Password Copied to Clipboard');
-        }).cath(err => {
-            console.error('clould not copy text', err);
-        })
-    } else{
-        alert('No Password to copy');
-    }
-})
